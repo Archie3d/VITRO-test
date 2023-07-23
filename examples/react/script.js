0cb2f3c40884661162ff0b7571de90355ca38bab
Arthur Benilov <arthur.benilov@gmail.com>
2023-07-23 23:51:28 +0100

    Update react example.

--- examples/react/script.js
+++ examples/react/script.js
@@ -3,29 +3,60 @@ import { React } from './react.js'
 //----------------------------------------------------------
 // JSX syntax expects Capitalized tags to be functions, so we need to
 // add a tralslation for those to the UI elements.
-const Label = (props) => {
-    return React.createElement("Label", props);
-};
-
-const Panel = (props) => {
-    return React.createElement("Panel", props);
-};
+const Label = (props) => { return React.createElement("Label", props); };
+const Panel = (props) => { return React.createElement("Panel", props); };
+const TextEditor = (prop) => { return React.createElement("TextEditor", prop); }
+const TextButton = (prop) => { return React.createElement("TextButton", prop); }
 
 //----------------------------------------------------------
 // Components
 
-const Row = (props) => {
+const Input = (props) => {
+
+    const editId = "new_item_text_editor";
+
+    const appendRow = () => {
+        if (props.onAdd) {
+            props.onAdd(view.getElementById(editId).getAttribute("text"));
+        }
+    }
+
     return (
-        <Label text={props.text} />
+        <Panel class="input">
+            <TextEditor id={editId} />
+            <TextButton text="Add" onclick={appendRow}/>
+        </Panel>
+    )
+}
+
+const Row = (props) => {
+    const deleteRow = () => {
+        if (props.onDelete) {
+            props.onDelete()
+        }
+    }
+
+    return (
+        <Panel class="row">
+            <Label class="row" text={props.text} />
+            <TextButton class="delete" text="X" onclick={deleteRow}/>
+        </Panel>
     )
 }
 
 const Table = (props) => {
+
+    const deleteRow = (index) => {
+        if (props.onDelete) {
+            props.onDelete(index);
+        }
+    }
+
     return (
-        <Panel>
+        <Panel class="scroll">
             {props.items.map((item, idx) => {
                 return (
-                    <Row text={item} />
+                    <Row text={item} onDelete={() => {deleteRow(idx)}}/>
                 )
             })}
         </Panel>
@@ -37,14 +68,32 @@ const Table = (props) => {
 
 var items = ["First", "Second", "Third", "Fourth", "Fifth"];
 
+
 //----------------------------------------------------------
 // Top-level application component.
 // The root tag returned by this component will be ignored, since
 // reconciliation starts at the first children level.
 const App = () => {
+
+    const update = () => {
+        //setTimeout(globalThis.render, 0);
+        globalThis.render();
+    }
+
+    const addRow = (text) => {
+        items.push(text);
+        update();
+    }
+
+    const deleteRow = (index) => {
+        items.splice(index, 1);
+        update();
+    }
+
     return (
         <view>
-            <Table items={items} />
+            <Input onAdd={addRow} />
+            <Table items={items} onDelete={deleteRow} />
         </view>
     )
 }
@@ -56,4 +105,5 @@ const App = () => {
 globalThis.render = () => {
     var app = App();
     React.reconcileChildren(app, view);
+    view.refresh();
 }
